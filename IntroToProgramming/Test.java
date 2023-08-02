d20209e195c870a3176974ff2c6f5bbdbedcedbf
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-08-02 22:36:54 +0000

    scopes and stuff

---
+++ IntroToProgramming/Test.java
@@ -0,0 +1,13 @@
+package IntroToProgramming;
+
+public class Test {
+    public static void main(String args[])  {  
+        int x;  
+        for (x = 0; x < 10; x++)  {  
+        //prints 0 to 9      
+        System.out.print(x+"\t");  
+        }  
+        //prints 10      
+        System.out.println(x);  
+    }  
+}
